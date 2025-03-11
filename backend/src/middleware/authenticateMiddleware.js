b669b0babbc41c64b36f9a2b98ce58138f927296
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2025-03-11 21:25:11 +0700

    fix: response status unauthorized

--- backend/src/middleware/authenticateMiddleware.js
+++ backend/src/middleware/authenticateMiddleware.js
@@ -5,24 +5,20 @@ const Authenticate = async (req, res, next) => {
     const SECRET_KEY = process.env.JWT_SECRET;
 
     if (!req.cookies || !req.cookies.accessToken) {
-      return res
-        .status(401)
-        .json({
-          status: 'unauthorized',
-          message: 'Access token tidak tersedia',
-        });
+      return res.status(401).json({
+        status: 'error',
+        message: 'Access token tidak tersedia',
+      });
     }
 
     const { accessToken } = req.cookies;
 
     jwt.verify(accessToken, SECRET_KEY, (err, decoded) => {
       if (err) {
-        return res
-          .status(401)
-          .json({
-            status: 'unauthorized',
-            message: 'Token tidak valid atau expired',
-          });
+        return res.status(401).json({
+          status: 'error',
+          message: 'Token tidak valid atau expired',
+        });
       }
 
       req.user = decoded;
